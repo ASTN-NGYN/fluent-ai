e438c0574848fcfc3c08f2894dfd63c648cbc0b3
Austin Nguyen <austin11@uw.edu>
2025-09-19 03:13:40 -0700

    feat: add pronunciation_assessment module that has a function that grades the pronunciation. It returns a JSON of total scores acorss different grading points. And also returns a list of phonemes and their accuracy scores

---
+++ backend/services/pronunciation_assessment.py
@@ -0,0 +1,63 @@
+import os
+import azure.cognitiveservices.speech as speechsdk
+from dotenv import load_dotenv
+from io import BytesIO
+
+# Configuration - Load environment variables
+load_dotenv()
+speech_key = os.getenv("AZURE_SPEECH_KEY")
+speech_region = os.getenv("AZURE_SPEECH_REGION")
+
+
+
+def assess_pronunciation(audio_file, reference_text: str, language: str = "en-EN"):
+    audio_bytes = audio_file.file.read()
+    audio_stream = speechsdk.audio.AudioConfig(stream=BytesIO(audio_bytes))
+    
+    speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=speech_region)
+    
+    pronunciation_config = speechsdk.PronunciationAssessmentConfig(
+        reference_text=reference_text,
+        grading_system=speechsdk.PronunciationAssessmentGradingSystem.HundredMark,
+        granularity=speechsdk.PronunciationAssessmentGranularity.Phoneme,
+        enable_miscue=True
+    )    
+    
+    pronunciation_config.enable_prosody_assessment()
+
+    speech_recognizer = speechsdk.SpeechRecognizer(
+        speech_config=speech_config,
+        audio_config=audio_stream,
+        language=language
+    )
+    pronunciation_config.apply_to(speech_recognizer)
+
+    result = speech_recognizer.recognize_once()
+    assessment_result = speechsdk.PronunciationAssessmentResult(result)
+
+    overall_scores = {
+        "accuracy": assessment_result.accuracy_score,
+        "completeness": assessment_result.completeness_score,
+        "fluency": assessment_result.fluency_score,
+        "pronunciation": assessment_result.pronunciation_score,
+        "prosody": assessment_result.prosody_score
+    }
+
+    word_breakdown = []
+    for word in assessment_result.words:
+        if word.phonemes:
+            phoneme_data = [{"phoneme": p.phoneme or "", "score": p.accuracy_score} for p in word.phonemes]
+        else:
+            phoneme_data = [{"phoneme": "", "score": word.accuracy_score}]
+            
+            word_breakdown.append({
+            "word": word.word,
+            "word_accuracy": word.accuracy_score,
+            "phoneme_scores": phoneme_data
+        })
+
+    return {
+        "overall_scores": overall_scores,
+        "word_breakdown": word_breakdown
+    }
+
